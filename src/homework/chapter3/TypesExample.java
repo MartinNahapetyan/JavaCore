65398d553fe02963a95a0e0919bc64192293db7e
Martin Nahapetyan <nahapetyanmartin45@gmail.com>
2023-08-27 17:06:21 +0400

    All my files

---
+++ src/homework/chapter3/TypesExample.java
@@ -0,0 +1,15 @@
+package homework.chapter3;
+
+public class TypesExample {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'Y';
+        char ch2;
+        ch2 = 'a';
+        char ch3;
+        ch3 = 'b';
+        System.out.println(ch3 +  " " +ch2 + " " + ch1);
+
+    }
+
+}
