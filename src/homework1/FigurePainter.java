4dca75ba9b91eb11bbf95714676a6e82c4d04edc
Martin Nahapetyan <nahapetyanmartin45@gmail.com>
2023-09-02 13:24:12 +0400

    All my files

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -2,10 +2,17 @@ package homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        int toxer = 5;
 
-        for (int i = 1; i <= toxer; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 5; i <=0 ; i--) {
+            for (int j = 0; j <=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
